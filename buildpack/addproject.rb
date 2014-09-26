0d7f639278becf506082574b90d46eddecd82144
Stephan Eberle <stephan@deltacity.org>
2014-09-26 20:16:08 +0200

    Cleaned up

--- buildpack/addproject.rb
+++ buildpack/addproject.rb
@@ -5,14 +5,14 @@ BASE_DIR = "/var/git/"
 
 HOSTNAME = `hostname -f`
 
-if Dir["#{BASE_DIR}${PROJECT_NAME}.git"] != nil
+if Dir["#{BASE_DIR}#{PROJECT_NAME}.git"] != nil
   puts "Project already exists."
   exit!
 end
 
-`git init --bare #{BASE_DIR}${PROJECT_NAME}.git`
-`ln -s /var/git/gitoku/buildpack/post-receive.rb #{BASE_DIR}${PROJECT_NAME}.git/hooks/post-receive`
+`git init --bare #{BASE_DIR}#{PROJECT_NAME}.git`
+`ln -s /var/git/gitoku/buildpack/post-receive.rb #{BASE_DIR}#{PROJECT_NAME}.git/hooks/post-receive`
 
 puts "Project created."
 puts ""
-puts "Use git remote add gitoku git@#{HOSTNAME}:#{BASE_DIR}${PROJECT_NAME}.git"
+puts "Use git remote add gitoku git@#{HOSTNAME}:#{BASE_DIR}#{PROJECT_NAME}.git"
